d030b5f2de14d142cb01e4692e0050a98ecd27ce
msekinci <mehmetserkanekinci@gmail.com>
2023-07-07 01:05:12 +0300

    Product Service Added

---
+++ src/Elasticsearch.API/DTOs/ResponseDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Elasticsearch.API.DTOs
+{
+    public class ResponseDto<T>
+	{
+		public T? Data { get; set; }
+		public List<string>? Errors { get; set; }
+		public HttpStatusCode Status { get; set; }
+
+		public static ResponseDto<T> Success(T data, HttpStatusCode status)
+		{
+			return new ResponseDto<T>
+			{
+				Data = data,
+				Status = status
+			};
+		}
+
+        public static ResponseDto<T> Fail(List<string> errors, HttpStatusCode status)
+        {
+            return new ResponseDto<T>
+            {
+                Errors = errors,
+                Status = status
+            };
+        }
+    }
+}
+
--- src/Elasticsearch.API/Models/Product.cs
+++ src/Elasticsearch.API/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using Nest;
+﻿using Elasticsearch.API.DTOs;
+using Nest;
 
 namespace Elasticsearch.API.Models
 {
@@ -12,6 +13,15 @@ namespace Elasticsearch.API.Models
 		public DateTime Created { get; set; }
 		public DateTime Updated { get; set; }
 		public ProductFeature? Feature { get; set; }
+
+		public ProductDto CreateDto() {
+			if (Feature == null)
+			{
+				return new ProductDto(Id, Name, Price, Stock, null);
+			}
+
+			return new ProductDto(Id, Name, Price, Stock, new ProductFeatureDto(Feature.Width, Feature.Height, Feature.Color));
+        }
 	}
 }
 
