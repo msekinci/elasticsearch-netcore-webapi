4a08f3a9ee46054a317b7cda6487709223973154
Mehmet Serkan Ekinci <mehmetserkanekinci@Mehmet-MacBook-Pro.local>
2023-07-06 17:25:49 +0300

    elastic client configuration created

---
+++ src/Elasticsearch.API/Extensions/Elasticsearch.cs
@@ -0,0 +1,22 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace Elasticsearch.API.Extensions
+{
+    public static class Elasticsearch
+	{
+		public static void AddElastic(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration.GetSection("Elasticsearch")["Url"]!;
+            var username = configuration.GetSection("Elasticsearch")["Username"]!;
+            var password = configuration.GetSection("Elasticsearch")["Password"]!;
+
+            var pool = new SingleNodeConnectionPool(new Uri(url));
+            var settings = new ConnectionSettings(pool);
+            settings.BasicAuthentication(username, password);
+            var client = new ElasticClient(settings);
+            services.AddSingleton(client);
+        }
+    }
+}
+
